9f5f709d81cf80ebc272be64f2267169f3f27a03
Muhammad Asadullah <muhammadd.asadullah@gmail.com>
2020-02-27 13:14:12 +0500

    app structure changed to navigation

---
+++ app/src/main/java/com/primesol/speakingreminder/android/ui/fragment/ReminderListFragment.kt
@@ -0,0 +1,20 @@
+package com.primesol.speakingreminder.android.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.primesol.speakingreminder.android.R
+
+class ReminderListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_reminder_list, container, false)
+    }
+
+}
