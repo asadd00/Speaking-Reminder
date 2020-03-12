27412407e19a24fc651917fb145f49a293143b8e
Muhammad Asadullah <muhammadd.asadullah@gmail.com>
2020-03-12 14:43:34 +0500

    app updater added

--- app/src/main/java/com/primesol/speakingreminder/android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/primesol/speakingreminder/android/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.primesol.speakingreminder.android.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.javiersantos.appupdater.AppUpdater
 import com.primesol.speakingreminder.android.R
 
 
@@ -11,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkForUpdates()
+    }
+
+    private fun checkForUpdates(){
+        val appUpdater = AppUpdater(this)
+        appUpdater.start()
+
     }
 }
